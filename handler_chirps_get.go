3474bb705c0b767136ee3a67cd7979dc5cd5632b
YuhengLinux <liuyuheng92@gmail.com>
2024-03-04 16:38:51 +0800

    add final handling for query parameters

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -33,20 +33,45 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 	// get all chirps from db
 	dbChirps, err := cfg.DB.GetChirps()
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't get chirps")
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps")
 		return
+	}
+	// retrieve author id from request query parameters
+	authorID := -1
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		// convert user ID to int
+		authorID, err = strconv.Atoi(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID")
+			return
+		}
+	}
+	// retrieve sort value from request query parameters, default to "asc"
+	sortDirection := "asc"
+	sortDirectionParam := r.URL.Query().Get("sort")
+	if sortDirectionParam == "desc" {
+		sortDirection = "desc"
 	}
 	// convert chirps from db struct to response struct
 	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
+		// skip adding of chirps if doesn't match author ID
+		if authorID != -1 && dbChirp.AuthorID != authorID {
+			continue
+		}
+
 		chirps = append(chirps, Chirp{
 			ID:       dbChirp.ID,
 			AuthorID: dbChirp.AuthorID,
 			Body:     dbChirp.Body,
 		})
 	}
-	// sort the chirps based on ID
+	// sort slice of Chirps based on chirp ID, sort direction is based on sortDirection value
 	sort.Slice(chirps, func(i, j int) bool {
+		if sortDirection == "desc" {
+			return chirps[i].ID > chirps[j].ID
+		}
 		return chirps[i].ID < chirps[j].ID
 	})
 	// send response with final sorted slice of chirps
